34ba0c555c5752e57fc5c3a78847d696fa135e66
4m1g0 <dev.4m1g0@gmail.com>
2015-02-01 06:51:26 +0100

    Showing tweets

--- tweetManager/tweet/views.py
+++ tweetManager/tweet/views.py
@@ -3,6 +3,7 @@ from django.http import HttpResponse
 import oauth2 as oauth
 import json
 import urllib
+import re
 
 CONSUMER_KEY = "b6VE4Huq6ypz76CUdNsGulqXj"
 CONSUMER_SECRET = "a7aJg9nyf1aEzg5Pck30fSiuDNU0ED2hzKLFSjne27eBVKXhdO"
@@ -10,9 +11,12 @@ ACCESS_KEY = "3008895899-nlv4Ni2JqEO7zieRxm7rtiw3PMZIIQVsmsXisjd"
 ACCESS_SECRET = "hTQd68Y5fRnrOwLBiy6IEbEnI6IuMSTaw3LrmCzs1vkHT"
 
 def processResponse(data):
-    context = {'markers':[],'hashtags':{},'users':{}}
+    context = {'markers':[],'hashtags':{},'users':{},'tweets':[]}
     hashtags = {}
-    for tweet in data:
+    if 'keyword' in data:
+        pattern = re.compile(re.escape(data['keyword']), re.IGNORECASE)
+        
+    for tweet in data['statuses']:
         # set geo position
         pos = tweet['coordinates']
         if pos != None and pos['type'] == 'Point':
@@ -28,12 +32,22 @@ def processResponse(data):
         name = tweet['user']['name']
         context['users'][name] = tweet['user']['id']
         
-    
+        # set tweet info
+        post = {}
+        if pattern:
+            post['text'] = pattern.sub('<b>'+data['keyword']+'</b>', tweet['text'])
+        else:
+            post['text'] = tweet['text']
+        post['screen_name'] = tweet['user']['screen_name']
+        post['name'] = tweet['user']['name']
+        post['profile_image'] = tweet['user']['profile_image_url_https']
+        post['retweet_count'] = tweet['retweet_count']
+        post['favorite_count'] = tweet['favorite_count']
+        context['tweets'].append(post)
 
     context['hashtags'] = sorted(hashtags.items(), key = lambda x: x[1], reverse=True)
     context['users'] = context['users'].items()
     
-    #context={'markers':[{'x':43.34583, 'y':-8.4108}]}
     return context
 
 consumer = oauth.Consumer(key=CONSUMER_KEY, secret=CONSUMER_SECRET)
@@ -43,7 +57,8 @@ client = oauth.Client(consumer, access_token)
 def index(request):
     url = "https://api.twitter.com/1.1/statuses/home_timeline.json"
     response, data = client.request(url)
-    return render(request, 'tweet/index.html', processResponse(json.loads(data)))
+    data = {'statuses': json.loads(data)}
+    return render(request, 'tweet/index.html', processResponse(data))
     
 def searchWord(request):
     if request.POST.has_key('search'):
@@ -51,7 +66,9 @@ def searchWord(request):
         params = {'q':search}
         url = "https://api.twitter.com/1.1/search/tweets.json?" + urllib.urlencode(params)
         response, data = client.request(url)
-        return render(request, 'tweet/index.html', processResponse(json.loads(data)['statuses']))
+        data = json.loads(data)
+        data['keyword'] = search
+        return render(request, 'tweet/index.html', processResponse(data))
     else:
         return render(request, 'tweet/index.html', {'error': True})
 
@@ -59,7 +76,7 @@ def searchUser(request, user):
     params = {'user_id':user}
     url = "https://api.twitter.com/1.1/statuses/user_timeline.json?" + urllib.urlencode(params)
     response, data = client.request(url)
-    print data
+    data = {'statuses': json.loads(data)}
     return render(request, 'tweet/index.html', processResponse(json.loads(data)))
     
 
